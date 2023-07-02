6ee3468e22cc54cd7035fc349c9087d672e02cb4
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-07-02 14:05:44 -0300

    finished home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,22 @@
-<<<<<<< HEAD
-import { GetStaticProps } from 'next';
-=======
-import { GetServerSideProps, GetStaticProps } from 'next';
-import { Fragment, useEffect, useState } from 'react';
-
->>>>>>> f7dcbdbd9bf2b1b69cd47783462520010922c224
 import Image from 'next/image';
+import Link from 'next/link';
+import Head from 'next/head';
+
+import { GetStaticProps } from 'next';
 import { useKeenSlider } from 'keen-slider/react';
 
 import * as Styles from '@/styles/pages/home';
-import { api } from '@/services/api';
-
-import Image_One from '../assets/2_explorer-t-shirt 1.png';
 
 import 'keen-slider/keen-slider.min.css';
-import Link from 'next/link';
+import { stripe } from '@/lib/stripe';
+import Stripe from 'stripe';
+import React from 'react';
 
-export default function Home({ data }) {
+interface DAtaProps {
+  products: Array<any>;
+}
+
+export default function Home({ products }: DAtaProps) {
   const [ sliderRef ] = useKeenSlider({
     slides: {
       perView: 3,
@@ -25,53 +25,47 @@ export default function Home({ data }) {
   });
 
   return (
-<<<<<<< HEAD
-    <Styles.HomeContainer ref={sliderRef} className='keen-slider'>
-      {data.map((product, index: number) => (
-        <Link key={index} href={`/product/${product.id}`}>
-          <Styles.Products className='keen-slider__slide'>
-            <Image src={Image_One} width={520} height={480} alt='' />
-
-            <footer>
-              <strong>{product.title}</strong>
-              <span>R$ {product.price}</span>
-            </footer>
-          </Styles.Products>
-        </Link>
-      ))}
-    </Styles.HomeContainer>
-=======
-    <Fragment>
+    <React.Fragment>
       <Head>
         <title>Home | Ignite Shop</title>
       </Head>
       <Styles.HomeContainer ref={sliderRef} className='keen-slider'>
-        {products.map((prod, index) => (
-          <Styles.Products key={index} href={`/products/${prod.id}`} className='keen-slider__slide'>
-            <Image src={Image_One} width={520} height={480} alt='' />
+        {products.map((product: any, index: number) => (
+          <Link key={index} href={`/product/${product.id}`} prefetch={false}>
+            <Styles.Products className='keen-slider__slide'>
+              <Image src={product.image} width={520} height={480} alt='' />
 
-            <footer>
-              <strong>{prod.name}</strong>
-              <span>{prod.price}</span>
-            </footer>
-          </Styles.Products>
+              <footer>
+                <strong>{product.title}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Styles.Products>
+          </Link>
         ))}
       </Styles.HomeContainer>
-    </Fragment>
->>>>>>> f7dcbdbd9bf2b1b69cd47783462520010922c224
+    </React.Fragment>
   )
 };
 
 export const getStaticProps: GetStaticProps = async() => {
-  const { data } = await api.get('data');
-  const products = data.map((product: any) => ({
-    id: product.id,
-    name: product.name,
-    price: new Intl.NumberFormat('pt-BR', {
-      style: 'currency',
-      currency: 'BRL'
-    }).format(product.price)
-  }))
+  const { data } = await stripe.products.list({
+    expand: [ 'data.default_price' ]
+  });
+
+  
+  const products = data.map(product => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      image: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100)
+    }
+  })
 
   return {
     props: {
@@ -81,24 +75,6 @@ export const getStaticProps: GetStaticProps = async() => {
   }
 }
 
-<<<<<<< HEAD
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { data } = await api.get('/data');
-
-  const response = data.map((products) => ({
-    ...products,
-    image: products.image[0],
-    price: new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(products.price)
-  }))
-
-  return {
-    props: {
-      data: response
-    },
-    revalidate: 60 * 60 * 2
-  }
-};
-=======
 /**
  * @augments SSR
  *  export const getServerSideProps: GetServerSideProps = async() => {
@@ -115,4 +91,3 @@ export const getStaticProps: GetStaticProps = async (context) => {
  *    }
  *  }
  */
->>>>>>> f7dcbdbd9bf2b1b69cd47783462520010922c224
